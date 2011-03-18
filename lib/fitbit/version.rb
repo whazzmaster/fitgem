9869ea42533c23ff08b63eaf251ded55933bab58
Zachery Moneypenny <whazzmaster@gmail.com>
2011-03-18 12:35:43 -0500

    Removing older files

--- lib/fitbit/version.rb
+++
@@ -1,3 +0,0 @@
-module Fitbit
-  VERSION = "0.0.1"
-end
--- spec/fitbit/fitbit_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Fitbit do
-  
-  it 'should expose the api_version' do
-    Fitbit::Client.api_version.should == "1"
-  end
-  
-end
