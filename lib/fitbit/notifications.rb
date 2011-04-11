068ca15596e3edb826fdb831e9aef8ebb3ef7f8c
Zachery Moneypenny <whazzmaster@gmail.com>
2011-04-11 15:38:17 -0500

    Adding initial notification support

--- lib/fitbit/notifications.rb
+++ lib/fitbit/notifications.rb
@@ -1,5 +1,40 @@
 module Fitbit
   class Client
     
+    def create_general_subscription(options={})
+      url = "/user/#{@user_id}/apiSubscriptions"
+      if options[:subscription_id]
+        url += "/#{options[:subscription_id]}"
+      end
+      if options[:subscription_response_format]
+        url += ".#{options[:subscription_response_format]}"
+      else
+        url += ".json"
+      end
+      # puts "Url: #{url}"
+      # resp = post(url)
+      # p resp
+    end
+    
+    def create_food_subscription
+      
+    end
+    
+    def create_activity_subscription
+      
+    end
+    
+    def create_sleep_subscription
+      
+    end
+    
+    def create_body_subscription
+      
+    end
+    
+    def remove_subscription
+      
+    end
+    
   end
 end
