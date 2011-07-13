27e1b05d41ab48d25747a0d183f25f6f77f18539
Zachery Moneypenny <whazzmaster@gmail.com>
2011-07-13 13:33:24 -0500

    Updated log_activity documentation, added distance units

--- lib/fitgem/activities.rb
+++ lib/fitgem/activities.rb
@@ -1,25 +1,25 @@
 module Fitgem
   class Client
-        
+
     # ==========================================
     #         Activity Retrieval Methods
     # ==========================================
     def activities_on_date(date)
       get("/user/#{@user_id}/activities/date/#{format_date(date)}.json")
     end
-    
+
     def frequent_activities()
       get("/user/#{@user_id}/activities/frequent.json")
     end
-    
+
     def recent_activities()
       get("/user/#{@user_id}/activities/recent.json")
     end
-    
+
     def favorite_activities()
       get("/user/#{@user_id}/activities/favorite.json")
     end
-    
+
     def activity(id, options ={})
       get("/activities/#{id}.json")
     end
@@ -27,33 +27,38 @@ module Fitgem
     # ==========================================
     #         Activity Update Methods
     # ==========================================
-    
+
     # The following values are REQUIRED when logging an activity:
-    #     options[:activityId]      =>  The activity id
-    #     options[:durationMillis]  =>  Activity duration in milliseconds 
-    #     options[:distance]        =>  Distance covered during activity date
+    #     options[:activityId]      =>  The id of the activity, directory activity or intensity level activity. See activity types at http://wiki.fitbit.com/display/API/API-Log-Activity for more information.
+    #     options[:durationMillis]  =>  Activity duration in milliseconds
     #     options[:startTime]       =>  Activity start time hours and minutes in the format HH:mm
+    #     options[:date]            =>  Set to today's date when not provided
+    #
+    # The following value is REQUIRED when logging directory activity, OPTIONAL otherwise
+    #     options[:distance]
+    #
     # The following values are OPTIONAL when logging an activity:
-    #     options[:date]            =>  set to today's date when not provided 
+    #     options[:distanceUnit]    => One of Fitgem::ApiDistanceUnit values
+    #     options[:manualCalories]  => Number of calories determined manually
     def log_activity(options)
       post("/user/#{@user_id}/activities.json", options)
     end
-    
+
     def add_favorite_activity(activity_id)
       post("/user/#{@user_id}/activities/log/favorite/#{activity_id}.json")
     end
-    
+
     # ==========================================
     #         Activity Removal Methods
-    # ==========================================    
+    # ==========================================
 
     def delete_logged_activity(activity_log_id)
       delete("/user/#{@user_id}/activities/#{activity_log_id}.json")
     end
-    
+
     def remove_favorite_activity(activity_id)
       delete("/user/#{@user_id}/activities/log/favorite/#{activity_id}.json")
-      
+
     end
   end
 end
--- lib/fitgem/units.rb
+++ lib/fitgem/units.rb
@@ -3,13 +3,51 @@ module Fitgem
     def self.US
       "en_US"
     end
-    
+
     def self.UK
       "en_UK"
     end
-    
+
     def self.METRIC
       ""
+    end
+  end
+
+  class ApiDistanceUnit
+    def self.centimeters
+      "Centimeter"
+    end
+
+    def self.feet
+      "Foot"
+    end
+
+    def self.inches
+      "Inch"
+    end
+
+    def self.kilometers
+      "Kilometer"
+    end
+
+    def self.meters
+      "Meter"
+    end
+
+    def self.miles
+      "Mile"
+    end
+
+    def self.millimeters
+      "Millimeter"
+    end
+
+    def self.steps
+      "Steps"
+    end
+
+    def self.yards
+      "Yards"
     end
   end
 end
