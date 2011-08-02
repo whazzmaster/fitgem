0c4a2a952bf6d76a3e347897e856e3cd7ab842b0
Zachery Moneypenny <whazzmaster@gmail.com>
2011-08-01 22:38:22 -0500

    Revert VERSION change, added ability to create foods

--- lib/fitgem.rb
+++ lib/fitgem.rb
@@ -4,4 +4,5 @@ require 'oauth'
 require 'fitgem/client'
 
 module Fitgem
+  VERSION = "0.3.0"
 end
--- lib/fitgem/foods.rb
+++ lib/fitgem/foods.rb
@@ -4,42 +4,42 @@ module Fitgem
     # ==========================================
     #          Food Retrieval Methods
     # ==========================================
-       
+
     def foods_on_date(date)
       get("/user/#{@user_id}/foods/log/date/#{format_date(date)}.json")
     end
-    
+
     def recent_foods()
       get("/user/#{@user_id}/foods/log/recent.json")
     end
-    
+
     def frequent_foods()
       get("/user/#{@user_id}/foods/log/frequent.json")
     end
-    
+
     def favorite_foods()
       get("/user/#{@user_id}/foods/log/favorite.json")
     end
-    
+
     def foods_units()
       get("/foods/units.json")
     end
-    
+
     def find_food(query_string)
       get("/foods/search.json?query=#{URI.escape(query_string)}")
     end
-    
+
     # ==========================================
     #           Food Update Methods
     # ==========================================
-        
+
     # Send the following required ID's in the options hash:
     #   options[:foodId]      => ID of the food to log
     #   options[:mealTypeId]  => ID of the meal to log the food for
     #   options[:unitId]      => ID of the unit to log with the food
     #      (typically retrieved via a previous call to get Foods (all, recent, frequent, favorite) or Food Units. )
     #   options[:amount]      => Amount consumed of the selected unit; a floating point number
-    #   options[:date]        => Log date in the format yyyy-MM-dd 
+    #   options[:date]        => Log date in the format yyyy-MM-dd
     def log_food(options)
       post("/user/#{@user_id}/foods/log.json", options)
     end
@@ -47,17 +47,32 @@ module Fitgem
     def add_favorite_food(food_id)
       post("/user/#{@user_id}/foods/log/favorite/#{food_id}.json")
     end
-    
+
     # ==========================================
     #           Food Removal Methods
-    # ==========================================    
-    
+    # ==========================================
+
     def delete_logged_food(food_log_id)
       delete("/user/#{@user_id}/foods/log/#{food_log_id}.json")
     end
-    
+
     def remove_favorite_food(food_id)
       delete("/user/#{@user_id}/foods/favorite/#{food_id}.json")
+    end
+
+    # ==========================================
+    #           Food Creation Methods
+    # ==========================================
+
+    # Create a food defined by the following items in the options hash:
+    #   options[:name] (required) => Food name
+    #   options[:defaultFoodMeasurementUnitId] (required) => Unit id; default measurement unit; full list of units could be retrieved via a previous calls to Get Food Units
+    #   options[:defaultServingSize] (required) => Size of the default serving; nutritional values should be provided for this serving size
+    #   options[:calories] (required) => Calories in the default serving size
+    #   options[:formType] (optional) => Form type; (LIQUID or DRY)
+    #   options[:description] (optional) => Description
+    def create_food(options)
+      post("/foods.json", options)
     end
   end
 end
