c1d239b7c59e342fcf70c8786204695bd78d71a0
Zachery Moneypenny <whazzmaster@gmail.com>
2011-08-19 10:33:10 -0500

    Added water log management
    	-> Can now log water consumption with log_water(options)
    	-> Can now delete a water log entry with delete_water_log(log_id)
    	-> Still getting stymied by the bad handling of nil responses by the oauth gem.  Need to shim to fix that.

--- lib/fitgem/water.rb
+++ lib/fitgem/water.rb
@@ -8,5 +8,31 @@ module Fitgem
       get("/user/#{@user_id}/foods/log/water/date/#{format_date(date)}.json")
     end
 
+
+    # ==========================================
+    #          Water Logging Methods
+    # ==========================================
+
+    # options[:amount]    Required
+    # options[:unit]      Optional    ("ml", "fl oz" or "cup")
+    # options[:date]      Required    yyyy-MM-dd format
+    def log_water(options)
+      unless options[:amount] && options[:date]
+        raise "Must include both an :amount and :date to log water"
+      end
+      post("/user/#{@user_id}/foods/log/water.json",options)
+    end
+
+    # ==========================================
+    #        Water Log Deletion Methods
+    # ==========================================
+
+    def delete_water_log(log_id)
+      begin
+        delete("/user/-/foods/log/water/#{log_id}.json")
+      rescue TypeError
+        # OAuth library responds badly to nil responses by fitbit's delete methods
+      end
+    end
   end
 end
