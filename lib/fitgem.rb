2fe153f4d3835770f459ccf76a1f5223e15af86a
Zachery Moneypenny <whazzmaster@gmail.com>
2011-11-23 16:57:39 -0600

    Add simple top-level docs for Fitgem

--- lib/fitgem.rb
+++ lib/fitgem.rb
@@ -1,7 +1,9 @@
 require 'json'
 require 'oauth'
-
 require 'fitgem/client'
 
+# Read/write data with the {http://www.fitbit.com Fitbit.com} OAuth API.
+#
+# @author Zachery Moneypenny
 module Fitgem
 end
