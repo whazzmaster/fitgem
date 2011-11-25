ebc1ca7ccbf26e28793a686b6e0100c8f3bd0172
Zachery Moneypenny <whazzmaster@gmail.com>
2011-11-25 17:49:20 -0600

    Updated documentation of time range method

--- lib/fitgem/time_range.rb
+++ lib/fitgem/time_range.rb
@@ -5,46 +5,57 @@ module Fitgem
     # :base_date and :end_date OR by using :base_date and a :period
     # (supported periods are 1d, 7d, 30d, 1w, 1m, 3m, 6m, 1y, max)
     #
-    # Supported values for resource_path are:
+    # Supported values for +resource_path+ are:
+    #
     # Food:
-    # /foods/log/caloriesIn
+    #   /foods/log/caloriesIn
     # 
     # Activity:
-    # /activities/log/calories
-    # /activities/log/steps
-    # /activities/log/distance
-    # /activities/log/floors
-    # /activities/log/elevation
-    # /activities/log/minutesSedentary
-    # /activities/log/minutesLightlyActive
-    # /activities/log/minutesFairlyActive
-    # /activities/log/minutesVeryActive
-    # /activities/log/activeScore
-    # /activities/log/activityCalories
+    #   /activities/log/calories
+    #   /activities/log/steps
+    #   /activities/log/distance
+    #   /activities/log/floors
+    #   /activities/log/elevation
+    #   /activities/log/minutesSedentary
+    #   /activities/log/minutesLightlyActive
+    #   /activities/log/minutesFairlyActive
+    #   /activities/log/minutesVeryActive
+    #   /activities/log/activeScore
+    #   /activities/log/activityCalories
     # 
     # Sleep:
-    # /sleep/minutesAsleep
-    # /sleep/minutesAwake
-    # /sleep/awakeningsCount
-    # /sleep/timeInBed
+    #   /sleep/minutesAsleep
+    #   /sleep/minutesAwake
+    #   /sleep/awakeningsCount
+    #   /sleep/timeInBed
     # 
     # Body:
-    # /body/weight
-    # /body/bmi
-    # /body/fat
+    #   /body/weight
+    #   /body/bmi
+    #   /body/fat
     #
-    #  Examples
+    # @param [String] resource_path The resource path to get data for
+    #   (see note above)
+    # @param [Hash] opts The options to specify date ranges, etc.
+    # @option opts [DateTime, Date, String] :base_date The start date of the period
+    # @option opts [DateTime, Date, String] :end_date The end date of the period
+    # @option opts [String] :period The period (valid values: 1d, 7d, 30d, 1w, 1m, 3m, 6m, 1y, max)
     #
-    #  * To get the floors traveled for the week of October 24th
-    #    data_by_time_range("/activities/log/floors", {:base_date => "2011-10-24", :period => "7d"})
+    # @return [Hash] Hash containing an array of objects that match your
+    #   request
     #
-    #  * To get all of the body weight logs from April 3rd until July 27th
-    #    data_by_time_range("/body/weight", {:base_date => "2011-03-03", :end_date => "2011-07-27"})
+    # @example To get the floors traveled for the week of October 24th
+    #   data_by_time_range("/activities/log/floors", {:base_date => "2011-10-24", :period => "7d"})
     #
-    def data_by_time_range(resource_path, options)
-      range_str = construct_date_range_fragment(options)
-      get("/user/#{@user_id}#{resource_path}/#{range_str}.json")
+    # @example To get all of the body weight logs from April 3rd until July 27th
+    #   data_by_time_range("/body/weight", {:base_date => "2011-03-03", :end_date => "2011-07-27"})
+    #
+    def data_by_time_range(resource_path, opts)
+      range = construct_date_range_fragment(opts)
+      get("/user/#{@user_id}#{resource_path}/#{range}.json")
     end
+
+    # protected
 
     def construct_date_range_fragment(options)
       range_str = "date/"
