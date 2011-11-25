aced031ca67f2dbaedccf59731b6830472ed2e81
Zachery Moneypenny <whazzmaster@gmail.com>
2011-11-25 15:52:38 -0600

    Add documentation and fix bug in social API
    There was a bug in the leader board method; the wrong endpoint was
    being targeted.

--- lib/fitgem/friends.rb
+++ lib/fitgem/friends.rb
@@ -1,17 +1,27 @@
 module Fitgem
   class Client
-
     # ==========================================
     #          Friend Retrieval Methods
     # ==========================================
+
+    # Get a list of the current user's friends
+    #
+    # @return [Array] List of friends, each of which is a Hash
+    #   containing friend information
     def friends
       get("/user/#{@user_id}/friends.json")
     end
 
+    # Get the weekly leaderboard of friends' activities
+    #
+    # @return [Hash] Friends' information
     def weekly_leaderboard
       leaderboard('7d')
     end
 
+    # Get the monthly leaderboard of friends' activities
+    #
+    # @return [Hash] Friends' information
     def monthly_leaderboard
       leaderboard('30d')
     end
@@ -19,20 +29,41 @@ module Fitgem
     # ==========================================
     #       Invitation Management Methods
     # ==========================================
-    def invite_friend(options)
-      unless options[:email] || options[:user_id]
-        raise InvalidArgumentError.new "add_friend hash argument must include :email or :user_id"
+
+    # Create an invite for a user to connect with the current user as a friend
+    #
+    # In order to invite a user, either an :email or a valid :userId
+    # must be supplied in the +opts+ param hash.
+    #
+    # @param [Hash] opts The invite data
+    # @option opts [String] :email Email address of user to invite
+    # @option opts [String] :userId User ID of the user to invite
+    #
+    # @return [Hash]
+    def invite_friend(opts)
+      unless opts[:email] || opts[:user_id]
+        raise InvalidArgumentError.new "invite_friend hash argument must include :email or :user_id"
       end
       translated_options = {}
-      translated_options[:invitedUserEmail] = options[:email] if options[:email]
-      translated_options[:invitedUserId] = options[:user_id] if options[:user_id]
+      translated_options[:invitedUserEmail] = opts[:email] if opts[:email]
+      translated_options[:invitedUserId] = opts[:user_id] if opts[:user_id]
       post("/user/#{@user_id}/friends/invitations.json", translated_options)
     end
 
+    # Accept a friend invite
+    #
+    # @param [String] requestor_id The ID of the requestor that sent the
+    #   friend request
+    # @return [Hash]
     def accept_invite(requestor_id)
       respond_to_invite(requestor_id, true)
     end
 
+    # Decline a friend invite
+    #
+    # @param [String] requestor_id The ID of the requestor that sent the
+    #   friend request
+    # @return [Hash]
     def decline_invite(requestor_id)
       respond_to_invite(requestor_id, false)
     end
@@ -40,7 +71,7 @@ module Fitgem
     private
 
     def leaderboard(range)
-      get("/user/#{@user_id}/friends/leaderboard/#{range}.json")
+      get("/user/#{@user_id}/friends/leaders/#{range}.json")
     end
 
     def respond_to_invite(requestor_id, does_accept)
