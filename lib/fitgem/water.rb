72b9ff170f4b34b8e9b8270afd15c0edcbcf960b
Zachery Moneypenny <whazzmaster@gmail.com>
2011-11-26 18:24:23 -0600

    Added documentation for water logging

--- lib/fitgem/water.rb
+++ lib/fitgem/water.rb
@@ -1,9 +1,14 @@
 module Fitgem
   class Client
-
     # ==========================================
     #          Water Retrieval Methods
     # ==========================================
+
+    # Get water log entries for the supplied date
+    #
+    # @param [DateTime, Date, String] date
+    # @return [Hash] Hash containing the summary of the days logs, and a
+    #   list of all individual entries
     def water_on_date(date)
       get("/user/#{@user_id}/foods/log/water/date/#{format_date(date)}.json")
     end
@@ -13,22 +18,33 @@ module Fitgem
     #          Water Logging Methods
     # ==========================================
 
-    # options[:amount]    Required
-    # options[:unit]      Optional    ("ml", "fl oz" or "cup")
-    # options[:date]      Required    yyyy-MM-dd format
-    def log_water(options)
-      unless options[:amount] && options[:date]
-        raise "Must include both an :amount and :date to log water"
+    # Log water consumption to fitbit
+    #
+    # @param [Hash] opts Water consumption data
+    # @option opts [Integer, Decimal, String] :amount Amount of water
+    #   consumed; if String must be in "X.X" format
+    # @option opts [String] :unit Unit of water consumed; valid values
+    #   are ("ml", "fl oz" or "cup")
+    # @option opts [DateTime, Date, String] :date Date of water
+    #   consumption
+    #
+    # @return [Hash] Summary of logged information
+    def log_water(opts)
+      unless opts[:amount] && opts[:date]
+        raise Fitgem::InvalidArgumentError, "Must include both an :amount and :date to log water"
       end
-      post("/user/#{@user_id}/foods/log/water.json",options)
+
+      opts[:date] = format_date(opts[:date])
+      post("/user/#{@user_id}/foods/log/water.json",opts)
     end
 
-    # ==========================================
-    #        Water Log Deletion Methods
-    # ==========================================
-
-    def delete_water_log(log_id)
-      delete("/user/-/foods/log/water/#{log_id}.json")
+    # Delete logged water consumption
+    #
+    # @param [Integer, String] water_log_id The id of previously logged
+    #   water consumption
+    # @return [Hash] Empty hash denotes success
+    def delete_water_log(water_log_id)
+      delete("/user/-/foods/log/water/#{water_log_id}.json")
     end
   end
 end
