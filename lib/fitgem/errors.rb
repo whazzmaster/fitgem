faeadef6cc276699a698dc1d0a216ac08dbee4cc
Zachery Moneypenny <whazzmaster@gmail.com>
2011-11-27 17:15:58 -0600

    Updated errors

--- lib/fitgem/errors.rb
+++ lib/fitgem/errors.rb
@@ -1,10 +1,13 @@
 module Fitgem
   class InvalidArgumentError < ArgumentError
   end
-  
+
   class UserIdError < Exception
   end
-  
-  class InvalidTimeRange < ArgumentError
+
+  class InvalidDateArgument < InvalidArgumentError
+  end
+
+  class InvalidTimeRange < InvalidArgumentError
   end
 end
