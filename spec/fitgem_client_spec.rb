6929131b58e2638b39653043ce50b1035b40347c
ota42y <ota42y@gmail.com>
2016-06-06 19:44:28 +0900

    add auth test

--- spec/fitgem_client_spec.rb
+++ spec/fitgem_client_spec.rb
@@ -4,12 +4,24 @@ RSpec.describe Fitgem::Client do
   let(:access_token) {
     double 'Access Token', :get => response, :request => response
   }
+  let(:consumer_key) { '12345' }
+  let(:consumer_secret) { '67890' }
   let(:client)       { Fitgem::Client.new({
-    :consumer_key => '12345',
-    :consumer_secret => '67890'
+    :consumer_key => consumer_key,
+    :consumer_secret => consumer_secret
   }) }
   let(:response)     { double :body => {:foo => :bar}.to_json, :status => 200 }
   let(:consumer)     { double 'Consumer' }
+  let(:redirect_uri) { 'http://example.com/redirect_url' }
+  let(:auth_url) { 'http://example.com/auth_url' }
+  let(:auth_code) { 'code' }
+  let(:scope) { 'activity nutrition heartrate location nutrition profile settings sleep social weight' }
+  let(:auth_header) {
+    {
+        Authorization: "Basic #{Base64.encode64("#{consumer_key}:#{consumer_secret}")}"
+    }
+  }
+  let(:auth_token) { 'token' }
 
   before :each do
     allow(OAuth2::Client).to receive(:new).with('12345', '67890', {
@@ -18,6 +30,19 @@ RSpec.describe Fitgem::Client do
       :authorize_url => "https://www.fitbit.com/oauth2/authorize"
     }).and_return(consumer)
     allow(OAuth2::AccessToken).to receive(:new).and_return(access_token)
+
+    allow(consumer).to receive_message_chain('auth_code.authorize_url').
+        with({scope: scope, redirect_uri: redirect_uri}).and_return(auth_url)
+    allow(consumer).to receive_message_chain('auth_code.get_token').
+        with(auth_code, {headers: auth_header, redirect_uri: redirect_uri}).and_return(auth_token)
+  end
+
+  it 'get authorize url' do
+    expect(client.authorize_url(scope, redirect_uri)).to eq auth_url
+  end
+
+  it 'get token'do
+    expect(client.get_token(auth_code, redirect_uri)).to eq auth_token
   end
 
   it 'returns JSON from the request' do
